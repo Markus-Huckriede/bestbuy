b1e31ca7fd300bf43c2ee9b5af6cf85fd7537030
Markus Huckriede <markushuckriede@MacBook-Air-von-Markus.fritz.box>
2025-07-08 10:24:54 +0200

    create Product class

---
+++ products.py
@@ -0,0 +1,68 @@
+class Product:
+    def __init__(self, name: str, price: float, quantity: int):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+        if name.strip() == "":
+            raise Exception("Enter a product name")
+        if price < 0:
+            raise Exception("Price cannot be negative")
+        if quantity < 0:
+            raise Exception("Quantity cannot be negative")
+
+
+    def get_quantity(self) -> int:
+        return self.quantity
+
+
+    def set_quantity(self, quantity):
+        if quantity < 0:
+            raise Exception("Quantity cannot be negative")
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+
+    def is_active(self) -> bool:
+        return self.active
+
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self) -> str:
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+    def buy(self, quantity) -> float:
+        if not self.active:
+            raise Exception(f"Product '{self.name}' is not in stock.")
+        if quantity <= 0:
+            raise Exception("Quantity cannot be negative.")
+        if quantity > self.quantity:
+            raise Exception(f"Not enough '{self.name}' in stock. Available: {self.quantity}")
+
+        total_price = self.price * quantity
+        self.quantity -= quantity
+        if self.quantity == 0:
+            self.deactivate()
+        return total_price
+
+
+
+bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+print(bose.buy(50))
+print(mac.buy(100))
+print(mac.is_active())
+
+print(bose.show())
+print(mac.show())
+
+bose.set_quantity(1000)
+print(bose.show())
